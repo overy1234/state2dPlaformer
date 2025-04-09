700a29dab644a22acd9afd14debbde5fc5a14268
overy1234 <overy1234@naver.com>
2025-04-09 17:52:41 +0900

    musa4

---
+++ GaemaMusa/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Enemy : Entity
+{
+    [Header("이동 정보")]
+    public float moveSpeed;
+    public float idleTime;
+
+
+    public EnemyStateMachine stateMachine { get; private set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        stateMachine = new EnemyStateMachine();
+    }
+
+
+    protected override void Update()
+    {
+        base.Update();
+
+        stateMachine.currentState.Update();
+    }
+
+
+
+}
