86c0625716d4c322f025afdffe7571deb5bf2c5f
overy1234 <overy1234@naver.com>
2025-04-22 14:00:06 +0900

    musa14

--- GaemaMusa/Assets/Scripts/Skills/Crystal_Skill.cs
+++ GaemaMusa/Assets/Scripts/Skills/Crystal_Skill.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Crystal_Skill : Skill
@@ -14,10 +16,24 @@ public class Crystal_Skill : Skill
     [SerializeField] private bool canMoveToEnemy;
     [SerializeField] private float moveSpeed;
 
+
+    [Header("Multi stacking crystal")]
+    [SerializeField] private bool canUseMultiStacks;
+    [SerializeField] private int amountOfStacks;
+    [SerializeField] private float multiStackCooldown;
+    [SerializeField] private float useTimeWindow;
+    [SerializeField] private List<GameObject> crystalLeft = new List<GameObject>();
+
+
     public override void UseSkill()
     {
         base.UseSkill();
 
+
+        if (CanUseMultiCrystal())
+            return;
+
+       
 
         if (currentCrystal == null)
         {
@@ -44,6 +60,64 @@ public class Crystal_Skill : Skill
 
     }
 
+    private bool CanUseMultiCrystal()
+    {
+        if(canUseMultiStacks)
+        {
+            if(crystalLeft.Count >0)
+            {
+
+                if (crystalLeft.Count == amountOfStacks)
+                    Invoke("ResetAbility", useTimeWindow);
+
+
+                cooldown = 0;
+                GameObject crystalToSwpan = crystalLeft[crystalLeft.Count - 1];
+                GameObject newCrystal = Instantiate(crystalToSwpan, player.transform.position, Quaternion.identity);
+
+                crystalLeft.Remove(crystalToSwpan);
+
+                newCrystal.GetComponent<Crystal_Skill_Controller>().SetupCrystal(crystalDuration, 
+                    canExplode,canMoveToEnemy, moveSpeed, FindClosestEnemy(newCrystal.transform));
+
+                if(crystalLeft.Count <= 0)
+                {
+                    //리필
+                    cooldown = multiStackCooldown;
+                    RefilCrystal();
+                }
+
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+
+    private void RefilCrystal()
+    {
+
+        int amountToAdd = amountOfStacks - crystalLeft.Count;
+
+        for(int i =0; i< amountToAdd; i++)
+        {
+            crystalLeft.Add(crystalPrefab);
+        }
+    }
+
+    private void ResetAbility()
+    {
+        if (cooldownTimer > 0)
+            return;
+
+        cooldownTimer = multiStackCooldown;
+        RefilCrystal();
+    }
+    
 
 
 }
