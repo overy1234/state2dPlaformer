da338260ea1f648c550f8f8d640ff8c9c888285c
overy1234 <overy1234@naver.com>
2025-04-25 13:20:22 +0900

    stat3

--- GaemaMusa/Assets/Scripts/CharacterStats.cs
+++ GaemaMusa/Assets/Scripts/CharacterStats.cs
@@ -2,10 +2,41 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
+    [Header("기본적인 스탯")]
+    public Stat strength; //힘
+    public Stat agility;  //회피
+    public Stat intelligence; //마법데미지
+    public Stat vitality; //체력
 
-    public Stat strength;
+    [Header("공격관련 스탯")]
     public Stat damage;
+    public Stat critChance;
+    public Stat critPower;
+
+
+    [Header("방어관련 스탯")]
     public Stat maxHealth;
+    public Stat armor;
+    public Stat evasion;
+    public Stat magicResistance;
+
+
+    [Header("마법 스탯")]
+    public Stat fireDamage;
+    public Stat iceDamage;
+    public Stat lightingDamage;
+
+
+    public bool isIgnited;
+    public bool isChilled;
+    public bool isShocked;
+
+
+
+    
+
+
+
 
     [SerializeField]
     private int currentHealth;
@@ -14,19 +45,94 @@ public class CharacterStats : MonoBehaviour
 
     protected virtual void Start()
     {
-        currentHealth = maxHealth.GetValue();
-                     
+
+        critPower.SetDefaultValue(150);
+        currentHealth = maxHealth.GetValue();                     
 
     }
 
     public virtual void DoDamage(CharacterStats _targetStats)
     {
+        if (TargetCanAvoidAttack(_targetStats))
+            return;
+
         int totalDamage = damage.GetValue() + strength.GetValue();
-        _targetStats.TakeDamage(totalDamage);
+
+
+        if (CanCrit())
+        {
+            totalDamage = CalculateCriticalDamage(totalDamage);
+            
+        }
+
+
+        totalDamage = CheckTargetArmor(_targetStats, totalDamage);
+        //_targetStats.TakeDamage(totalDamage);
+        DoMagicalDamage(_targetStats);
+
+    }
+
+    public virtual void DoMagicalDamage(CharacterStats _targetStats)
+    {
+        int _fireDamage = fireDamage.GetValue();
+        int _iceDamage = iceDamage.GetValue();
+        int _lightingDamage = lightingDamage.GetValue();
+
+        int totalMagicalDamage = _fireDamage + _iceDamage + _lightingDamage + intelligence.GetValue();
+        totalMagicalDamage = CheckTargetResistance(_targetStats, totalMagicalDamage);
+
+        _targetStats.TakeDamage(totalMagicalDamage);
+
+
+    }
+
+    private  int CheckTargetResistance(CharacterStats _targetStats, int totalMagicalDamage)
+    {
+        totalMagicalDamage -= _targetStats.magicResistance.GetValue() + (_targetStats.intelligence.GetValue() * 3);
+        totalMagicalDamage = Mathf.Clamp(totalMagicalDamage, 0, int.MaxValue);
+        return totalMagicalDamage;
+    }
+
+    public void ApplyAilments(bool _ignite, bool _chill, bool _shock)
+    {
+        if (isIgnited || isChilled || isShocked)
+        {
+            return;
+        }
+
+        isIgnited = _ignite;
+        isChilled = _chill;
+        isShocked = _shock;
     }
 
 
+
+
+    private  int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
+    {
+        totalDamage -= _targetStats.armor.GetValue();
+        totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);
+        return totalDamage;
+    }
+
+    private bool TargetCanAvoidAttack(CharacterStats _targetStats)
+    {
+        int totalEvasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+
+        if (Random.Range(0, 100) < totalEvasion)
+        {
+            return true;
+
+        }
+        return false;
+    }
+
+
+
     
+
+
+
     public virtual void TakeDamage(int _damage)
     {
         currentHealth -= _damage;
@@ -41,5 +147,30 @@ public class CharacterStats : MonoBehaviour
     {
 
     }
+
+
+
+    private bool CanCrit()
+    {
+        int totalCriticalChance = critChance.GetValue() + agility.GetValue();
+
+        if(Random.Range(0,100) <= totalCriticalChance)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private int CalculateCriticalDamage(int _damage)
+    {
+        float totalCritPower = (critPower.GetValue() + strength.GetValue()) * 0.01f;  
+        float critDamage = _damage * totalCritPower;
+        
+
+        return Mathf.RoundToInt(critDamage);
+    }
+
+
 
 }
